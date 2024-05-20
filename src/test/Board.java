0c409a4251482ff04af8ce0a34a44433d24c9473
ItayFrisk <itay.manevych@gmail.com>
2024-05-20 21:28:06 +0300

    Created board class

--- src/test/Board.java
+++ src/test/Board.java
@@ -2,5 +2,32 @@ package test;
 
 
 public class Board {
+    Tile[][] board;
+    final int rows;
+    final int cols;
 
+    private static Board boardInstance;
+    
+    private Board() {
+        rows = 15;
+        cols = 15;
+        board = new Tile[rows][cols];
+        boardInstance = null;
+    }
+    
+    Board getBoard() {
+        if (boardInstance == null) {
+            boardInstance = new Board();
+        }
+        return boardInstance;
+    }
+    Tile[][] getTiles() {
+        Tile[][] copyBoard = new Tile[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                copyBoard[i][j] = board[i][j];
+            }
+        }
+        return copyBoard;
+    }
 }
