f7d34dfd029bfd8bb634cbb2dd70065e2f32f497
ItayFrisk <itay.manevych@gmail.com>
2024-05-21 17:06:35 +0300

    Create getWords method

--- src/test/Board.java
+++ src/test/Board.java
@@ -1,13 +1,15 @@
 package test;
 
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class Board {
     Tile[][] board;
     final int rows;
     final int cols;
-
     private static Board boardInstance;
-
+    HashSet<String> stringHashSet;
     private Board() {
         rows = 15;
         cols = 15;
@@ -29,7 +31,7 @@ public class Board {
         return copyBoard;
     }
 
-    boolean checkWordLocation(Word word) {
+    private boolean checkWordLocation(Word word) {
         int wordLength = word.getTiles().length;
         int wordRow = word.getRow();
         int wordCol = word.getCol();
@@ -45,7 +47,7 @@ public class Board {
         }
     }
 
-    boolean checkFirstPlacement(Word word) {
+    private boolean checkFirstPlacement(Word word) {
         int wordLength = word.getTiles().length;
         int wordRow = word.getRow();
         int wordCol = word.getCol();
@@ -73,7 +75,7 @@ public class Board {
         return true;
     }
 
-    boolean checkOverlappingTiles(Word word) {
+    private boolean checkOverlappingTiles(Word word) {
         Tile[] wordTiles = word.getTiles();
         int wordLength = wordTiles.length;
         int wordRow = word.getRow();
@@ -93,19 +95,19 @@ public class Board {
         return true;
     }
 
-    boolean checkAdjacentTiles(Word word) {
+    private boolean checkAdjacentTiles(Word word) {
         Tile[] wordTiles = word.getTiles();
         int wordLength = wordTiles.length;
         int wordRow = word.getRow();
         int wordCol = word.getCol();
         for (int i = 0; i < wordLength; i++) {
             if (word.isVertical()) {
-                if ((wordCol < 14 && board[wordRow + i][wordCol + 1] != null)
+                if ((wordCol < cols - 1 && board[wordRow + i][wordCol + 1] != null)
                     || (wordCol > 0 && board[wordRow + i][wordCol - 1] != null)) {
                         return true;
                 }
             } else {
-                if ((wordRow < 14 && board[wordRow + 1][wordCol + i] != null)
+                if ((wordRow < rows - 1 && board[wordRow + 1][wordCol + i] != null)
                     || (wordCol < 0 && board[wordRow - 1][wordCol + i] != null)) {
                         return true;
                 }
@@ -118,5 +120,112 @@ public class Board {
         return checkWordLocation(word)
                 && checkFirstPlacement(word)
                 && (checkOverlappingTiles(word) || checkAdjacentTiles(word));
+    }
+
+    boolean dictionaryLegal() {
+        return true;
+    }
+    private int findStartOfWord(int row, int col, boolean isVertical) {
+        int i = 0;
+        int start;
+        if (isVertical) {
+            start = col;
+            while (i < col && board[row][col - i] != null) {
+                start--;
+                i++;
+            }
+        } else {
+            start = row;
+            while (i < row && board[row - i][col] != null) {
+                start--;
+                i++;
+            }
+        }
+        return start;
+    }
+    private int findEndOfWord(int row, int col, boolean isVertical) {
+        int i = 0;
+        int end;
+        if (isVertical) {
+            end = col;
+            while (i < cols - col && board[row][col + i] != null) {
+                end++;
+                i++;
+            }
+        } else {
+            end = row;
+            while (i < rows - row && board[row + i][col] != null) {
+                end++;
+                i++;
+            }
+        }
+        return end;
+    }
+    private void addInsertedWord(Word word, ArrayList<Word> wordsArray) {
+        Tile[] wordTiles = word.getTiles();
+        int wordLength = wordTiles.length;
+        int wordRow = word.getRow();
+        int wordCol = word.getCol();
+        Tile[] newWordTiles = new Tile[wordLength];
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < wordLength; i++) {
+            if (wordTiles[i].letter == '_') {
+                if(word.isVertical()) {
+                    newWordTiles[i] = board[wordRow + i][wordCol];
+                    sb.append(newWordTiles[i].letter);
+                } else {
+                    newWordTiles[i] = board[wordRow][wordCol + i];
+                    sb.append(newWordTiles[i].letter);
+                }
+            } else {
+                newWordTiles[i] = wordTiles[i];
+                sb.append(newWordTiles[i].letter);
+            }
+        }
+        wordsArray.add(new Word(newWordTiles, wordRow, wordCol, word.isVertical()));
+        stringHashSet.add(sb.toString());
+    }
+    private void addCrossword(Word word, ArrayList<Word> wordsArray, int index, boolean isVertical) {
+        int wordRow = word.getRow();
+        int wordCol = word.getCol();
+
+        int start = isVertical
+                ? findStartOfWord(wordRow + index, wordCol, isVertical)
+                : findStartOfWord(wordRow, wordCol + index, isVertical);
+        int end = isVertical
+                ? findEndOfWord(wordRow + index, wordCol, isVertical)
+                : findEndOfWord(wordRow, wordCol + index, isVertical);
+
+        Tile[] newWordTiles = new Tile[end - start + 1];
+        StringBuilder sb = new StringBuilder();
+
+        for (int j = 0, startTemp = start; startTemp <= end; j++, startTemp++) {
+            newWordTiles[j] = isVertical
+                    ? board[wordRow + index][startTemp]
+                    : board[startTemp][wordCol + index];
+            sb.append(newWordTiles[j].letter);
+        }
+
+        String wordString = sb.toString();
+        if (!stringHashSet.contains(wordString)) {
+            stringHashSet.add(wordString);
+            wordsArray.add(new Word(
+                    newWordTiles,
+                    isVertical ? wordRow + index : start,
+                    isVertical ? start : wordCol + index,
+                    !isVertical
+            ));
+        }
+    }
+
+    ArrayList<Word> getWords(Word word) {
+        ArrayList<Word> wordsArray = new ArrayList<Word>();
+        Tile[] wordTiles = word.getTiles();
+        int wordLength = wordTiles.length;
+        addInsertedWord(word, wordsArray);
+        for (int i = 0; i < wordLength; i++) {
+            addCrossword(word, wordsArray, i, word.isVertical());
+        }
+        return wordsArray;
     }
 }
