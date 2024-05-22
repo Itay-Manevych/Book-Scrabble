b9708fe1f984d4cde7be666cf9ba1821d6e4acc7
ItayFrisk <itay.manevych@gmail.com>
2024-05-22 17:14:13 +0300

    Created CacheManager

--- src/test/CacheManager.java
+++ src/test/CacheManager.java
@@ -1,8 +1,30 @@
 package test;
 
 
+import java.util.HashSet;
+
 public class CacheManager {
 	
-	
+	int size;
+    CacheReplacementPolicy crp;
+    HashSet<String> cache;
 
+    public CacheManager (int size, CacheReplacementPolicy crp) {
+        this.size = size;
+        this.crp = crp;
+        cache = new HashSet<>();
+    }
+
+    public boolean query (String word) {
+        return cache.contains(word);
+    }
+
+    public void add (String word) {
+        if (cache.size() == size) {
+            String wordToRemove = crp.remove();
+            cache.remove(wordToRemove);
+        }
+        crp.add(word);
+        cache.add(word);
+    }
 }
