6113f9d9fac7746cba7f4ef98b6eccb9fcbffc0f
ItayFrisk <itay.manevych@gmail.com>
2024-05-22 13:33:36 +0300

    fixed board bugs (will refactor)

--- src/test/Board.java
+++ src/test/Board.java
@@ -122,7 +122,32 @@ public class Board {
         }
         return true;
     }
+    private boolean checkAdjacentTile(Tile tile, int row, int col) {
+        if (col < 14 && board[row][col + 1] != null) {
+            return true;
+        } else if (col == 14 && tile.letter == '_' && board[row][col] != null) {
+            return true;
+        }
 
+        if (col > 0 && board[row][col - 1] != null) {
+            return true;
+        } else if (col == 0 && tile.letter == '_' && board[row][col] != null) {
+            return true;
+        }
+
+        if (row < 14 && board[row + 1][col] != null) {
+            return true;
+        } else if (row == 14 && tile.letter == '_' && board[row][col] != null) {
+            return true;
+        }
+
+        if (row > 0 && board[row - 1][col] != null) {
+            return true;
+        } else if (col == 0 && tile.letter == '_' && board[row][col] != null) {
+            return true;
+        }
+        return false;
+    }
     private boolean checkAdjacentTiles(Word word) {
         Tile[] wordTiles = word.getTiles();
         int wordLength = wordTiles.length;
@@ -130,41 +155,89 @@ public class Board {
         int wordCol = word.getCol();
         for (int i = 0; i < wordLength; i++) {
             if (word.isVertical()) {
-                if ((wordCol < cols - 1 && board[wordRow + i][wordCol + 1] != null)
-                        || (wordCol > 0 && board[wordRow + i][wordCol - 1] != null)) {
-                    return true;
-                }
+               if(checkAdjacentTile(wordTiles[i], wordRow + i, wordCol)) {
+                   return true;
+               }
             } else {
-                if ((wordRow < rows - 1 && board[wordRow + 1][wordCol + i] != null)
-                        || (wordCol < 0 && board[wordRow - 1][wordCol + i] != null)) {
+                if(checkAdjacentTile(wordTiles[i] ,wordRow, wordCol + i)) {
                     return true;
                 }
             }
         }
         return false;
     }
-
+    boolean isWordEmpty(Word word) {
+        Tile[] wordTiles = word.getTiles();
+        int wordLength = wordTiles.length;
+        for (int i = 0; i < wordLength; i++) {
+            Tile currentTile = wordTiles[i];
+            if (currentTile.letter != '_') {
+                return false;
+            }
+        }
+        return true;
+    }
+    boolean wordTileCount(Word word) {
+        Tile[] wordTiles = word.getTiles();
+        int wordLength = wordTiles.length;
+        for (int i = 0; i < wordLength; i++) {
+            Tile currentTile = wordTiles[i];
+            if (currentTile == null) {
+                return false;
+            }
+        }
+        return true;
+    }
+    boolean checkBoardEmpty() {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (board[i][j] != null) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
     boolean boardLegal(Word word) {
-        return checkWordLocation(word)
-                && checkFirstPlacement(word)
-                && (checkOverlappingTiles(word) || checkAdjacentTiles(word));
+        if (!wordTileCount(word)) {
+            return false;
+        }
+        if (isWordEmpty(word)) {
+            return false;
+        }
+        if (!checkWordLocation(word)) {
+            return false;
+        }
+        if (!checkFirstPlacement(word)) {
+            return false;
+        }
+        if (checkAdjacentTiles(word)) {
+            if(!checkOverlappingTiles(word)) {
+                return false;
+            } else {
+                return true;
+            }
+        } else if (checkBoardEmpty()) {
+            return true;
+        }
+        return false;
     }
 
     boolean dictionaryLegal(Word word) {
         return true;
     }
     private int findStartOfWord(int row, int col, boolean isVertical) {
-        int i = 1;
+        int i = 0;
         int start;
         if (isVertical) {
             start = col;
-            while (i < cols - col && board[row][col - i] != null) {
+            while (i < col && board[row][col - i - 1] != null) {
                 start--;
                 i++;
             }
         } else {
             start = row;
-            while (i < rows - row && board[row - i][col] != null) {
+            while (i < row && board[row - i - 1][col] != null) {
                 start--;
                 i++;
             }
@@ -173,17 +246,17 @@ public class Board {
     }
 
     private int findEndOfWord(int row, int col, boolean isVertical) {
-        int i = 1;
+        int i = 0;
         int end;
         if (isVertical) {
             end = col;
-            while (i < cols - col && board[row][col + i] != null) {
+            while (i + 1 < cols - col && board[row][col + i + 1] != null) {
                 end++;
                 i++;
             }
         } else {
             end = row;
-            while (i < rows - row && board[row + i][col] != null) {
+            while (i + 1 < rows - row && board[row + i + 1][col] != null) {
                 end++;
                 i++;
             }
@@ -251,18 +324,204 @@ public class Board {
             ));
         }
     }
+    boolean checkSubWord(ArrayList<Word> wordArrayList, Word word) {
+        Tile[] wordTiles = word.getTiles();
+        int wordLength = wordTiles.length;
+        int wordRow = word.getRow();
+        int wordCol = word.getCol();
+
+        if (word.isVertical()) {
+            if ((wordRow > 0 && board[wordRow - 1][wordCol] != null)
+            || (wordRow == 0 && wordTiles[0].letter == '_' && board[wordRow][wordCol] != null)) {
+                int start = findStartOfWord(wordRow, wordCol, !word.isVertical());
+                int end = findEndOfWord(wordRow, wordCol, !word.isVertical());
+                if (end >= wordRow && board[wordRow][wordCol] == null) {
+                    end = wordRow + wordLength - 1;
+                }
+                if (start == end) {
+                    return false;
+                }
+
+                Tile[] newWordTiles = new Tile[end - start + 1];
+                StringBuilder sb = new StringBuilder();
+                int j = 0;
+                int k = 0;
+                int startTemp = start;
+                boolean flag = false;
+                while (startTemp <= end) {
+                    if (board[startTemp][wordCol] == null) {
+                        newWordTiles[j] = wordTiles[k];
+                        sb.append(newWordTiles[j].letter);
+                        k++;
+                        flag = true;
+                    } else {
+                        newWordTiles[j] = board[startTemp][wordCol];
+                        sb.append(newWordTiles[j].letter);
+                        if (flag) {
+                            k++;
+                        }
+                    }
+                    j++;
+                    startTemp++;
+                }
+                String wordString = sb.toString();
+                if (!stringHashSet.contains(wordString)) {
+                    stringHashSet.add(wordString);
+                    wordArrayList.add(new Word(newWordTiles,start,wordCol,word.isVertical()));
+                }
+                return true;
+            }
+            else if ((wordRow + wordLength < 14 && board[wordRow + wordLength][wordCol] != null)
+            || (wordRow + wordLength == 14 &&  wordTiles[wordLength - 1].letter == '_' && board[wordRow + wordLength][wordCol] != null)) {
+                int start = findStartOfWord(wordRow, wordCol, !word.isVertical());
+                int end = findEndOfWord(wordRow, wordCol, !word.isVertical());
+                if (end >= wordRow && board[wordRow][wordCol] == null) {
+                    int i = 0;
+                    end = wordLength - 1;
+                    while (wordLength + i < rows - wordRow && board[wordRow + wordLength + i][wordCol] != null) {
+                        i++;
+                        end++;
+                    }
+                }
+                if (start == end) {
+                    return false;
+                }
+
+                Tile[] newWordTiles = new Tile[end - start + 1];
+                StringBuilder sb = new StringBuilder();
+                int j = 0;
+                int k = 0;
+                int startTemp = start;
+                boolean flag = false;
+                while (startTemp <= end) {
+                    if (board[startTemp][wordCol] == null) {
+                        newWordTiles[j] = wordTiles[k];
+                        sb.append(newWordTiles[j].letter);
+                        k++;
+                        flag = true;
+                    } else {
+                        newWordTiles[j] = board[startTemp][wordCol];
+                        sb.append(newWordTiles[j].letter);
+                        if (flag) {
+                            k++;
+                        }
+                    }
+                    j++;
+                    startTemp++;
+                }
+                String wordString = sb.toString();
+                if (!stringHashSet.contains(wordString)) {
+                    stringHashSet.add(wordString);
+                    wordArrayList.add(new Word(newWordTiles,start,wordCol,word.isVertical()));
+                }
+                return true;
+            }
+            return false;
+        } else {
+            if ((wordCol > 0  && board[wordRow][wordCol - 1] != null)
+            || (wordCol == 0 && wordTiles[0].letter == '_') && board[wordRow][wordCol] != null) {
+                int start = findStartOfWord(wordRow, wordCol, !word.isVertical());
+                int end = findEndOfWord(wordRow, wordCol, !word.isVertical());
+                if (end >= wordCol && board[wordRow][wordCol] == null) {
+                    end = wordCol + wordLength - 1;
+                }
+                if (start == end) {
+                    return false;
+                }
+
+                Tile[] newWordTiles = new Tile[end - start + 1];
+                StringBuilder sb = new StringBuilder();
+                int j = 0;
+                int k = 0;
+                int startTemp = start;
+                boolean flag = false;
+                while (startTemp <= end) {
+                    if (board[wordRow][startTemp] == null) {
+                        newWordTiles[j] = wordTiles[k];
+                        sb.append(newWordTiles[j].letter);
+                        k++;
+                        flag = true;
+                    } else {
+                        newWordTiles[j] = board[wordRow][startTemp];
+                        sb.append(newWordTiles[j].letter);
+                        if (flag) {
+                            k++;
+                        }
+                    }
+                    j++;
+                    startTemp++;
+                }
+                String wordString = sb.toString();
+                if (!stringHashSet.contains(wordString)) {
+                    stringHashSet.add(wordString);
+                    wordArrayList.add(new Word(newWordTiles,wordRow,start,word.isVertical()));
+                }
+                return true;
+            } else if ((wordCol + wordLength < 14 && board[wordRow][wordCol + wordLength] != null)
+                    || ((wordCol + wordLength == 14 && wordTiles[wordLength - 1].letter == '_') && board[wordRow][wordCol] != null)) {
+                int start = findStartOfWord(wordRow, wordCol, !word.isVertical());
+                int end = findEndOfWord(wordRow, wordCol, !word.isVertical());
+                if (end >= wordCol && board[wordRow][wordCol] == null) {
+                    int i = 0;
+                    end = wordLength - 1;
+                    while (wordLength + i < cols - wordCol && board[wordRow][wordCol + wordLength + i] != null) {
+                        i++;
+                        end++;
+                    }
+                }
+                if (start == end) {
+                    return false;
+                }
+
+                Tile[] newWordTiles = new Tile[end - start + 1];
+                StringBuilder sb = new StringBuilder();
+                int j = 0;
+                int k = 0;
+                int startTemp = start;
+                boolean flag = false;
+                while (startTemp <= end) {
+                    if (board[wordRow][startTemp] == null) {
+                        newWordTiles[j] = wordTiles[k];
+                        sb.append(newWordTiles[j].letter);
+                        k++;
+                        flag = true;
+                    } else {
+                        newWordTiles[j] = board[wordRow][startTemp];
+                        sb.append(newWordTiles[j].letter);
+                        if (flag) {
+                            k++;
+                        }
+                    }
+                    j++;
+                    startTemp++;
+                }
+                String wordString = sb.toString();
+                if (!stringHashSet.contains(wordString)) {
+                    stringHashSet.add(wordString);
+                    wordArrayList.add(new Word(newWordTiles,wordRow,wordCol,word.isVertical()));
+                }
+                return true;
+            }
+            return false;
+        }
+    }
     ArrayList<Word> getWords(Word word) {
         ArrayList<Word> wordsArray = new ArrayList<Word>();
         Tile[] wordTiles = word.getTiles();
         int wordLength = wordTiles.length;
-        addInsertedWord(word, wordsArray);
         for (int i = 0; i < wordLength; i++) {
             addCrossword(word, wordsArray, i, word.isVertical());
         }
+        if(!checkSubWord(wordsArray,word)) {
+            addInsertedWord(word, wordsArray);
+        }
         return wordsArray;
     }
-    private int calculateTileScore(int row, int col) {
+    private int calculateTileScore(Word word, int row, int col, int index) {
         Tile currentTile = board[row][col];
+        if (currentTile == null) {
+            currentTile = word.getTiles()[index];
+        }
         BonusType currentBonus = bonuses[row][col];
         if (row == rows / 2 && col == cols / 2) {
             bonuses[row][col] = null;
@@ -286,25 +545,35 @@ public class Board {
         int score = 0;
         boolean doubleWordMultiplierFlag = false;
         boolean tripleWordMultiplierFlag = false;
+        int doubleWordMultiplierCount = 0;
+        int tripleWordMultiplierCount = 0;
 
         for (int i = 0; i < wordLength; i++) {
             int row = word.isVertical() ? wordRow + i : wordRow;
             int col = word.isVertical() ? wordCol : wordCol + i;
 
             BonusType currentBonus = bonuses[row][col];
-            score += calculateTileScore(row, col);
+            score += calculateTileScore(word, row, col, i);
 
             if (currentBonus == BonusType.DOUBLE_WORD_SCORE) {
                 doubleWordMultiplierFlag = true;
+                doubleWordMultiplierCount++;
             } else if (currentBonus == BonusType.TRIPLE_WORD_SCORE) {
                 tripleWordMultiplierFlag = true;
+                tripleWordMultiplierCount++;
             }
         }
 
         if (doubleWordMultiplierFlag) {
-            score *= 2;
+            while (doubleWordMultiplierCount > 0) {
+                score *= 2;
+                doubleWordMultiplierCount--;
+            }
         } if (tripleWordMultiplierFlag) {
-            score *= 3;
+            while (tripleWordMultiplierCount > 0) {
+                score *= 3;
+                tripleWordMultiplierCount--;
+            }
         }
 
         return score;
