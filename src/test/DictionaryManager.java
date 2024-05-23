a0bd86ed2ef3f7a2cc6480fb0deac286bd5be3bf
ItayFrisk <itay.manevych@gmail.com>
2024-05-23 22:08:19 +0300

    Fixed DictionaryManager to be singeltone

--- src/test/DictionaryManager.java
+++ src/test/DictionaryManager.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 public class DictionaryManager {
     HashMap<String, test.Dictionary> map;
     private static DictionaryManager dictionaryManagerInstance;
-    public DictionaryManager() {
+    private DictionaryManager() {
         map = new HashMap<>();
         dictionaryManagerInstance = null;
     }
